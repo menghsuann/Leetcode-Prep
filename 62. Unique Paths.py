3956b48e7218c1732478f4d48fc3faeb7428bd8c
Meng Hsuan Tsai <menghsuantsai@berkeley.edu>
2021-04-04 12:12:01 -0400

    Create 62. Unique Paths.py

---
+++ 62. Unique Paths.py
@@ -0,0 +1,15 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        #extreme case
+        if m == n == 1:
+            return 1
+        #set up recursion conditions
+        dp = [[1]*m]*n
+        #for loop: sum of left and up; can only move either down or right
+        for row in range(1, len(dp)):
+            for col in range(1, len(dp[row])):
+                dp[row][col] = dp[row-1][col] + dp[row][col-1]
+        #the final right down corner would be our anwser
+        return dp[-1][-1]
+  #dynamic programming
+  #https://www.youtube.com/watch?v=gp_wfYhjALw
