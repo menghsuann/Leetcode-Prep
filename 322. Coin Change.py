f2c87b9769cb08e57a2574daec85f2dffc48e7f6
Coco Tsai <menghsuantsai@berkeley.edu>
2021-04-06 21:41:15 -0400

    Update 322. Coin Change.py

--- 322. Coin Change.py
+++ 322. Coin Change.py
@@ -5,13 +5,13 @@ class Solution:
         #initial the first value 0
         dp[0] = 0
         #starting from the second value to the last #
-        #the number of coin is smaller than the amount we want to target
+        #the number of coin is smaller than the targeted amount
         #sub-problem for every i: what is the minimum way of change
         for i in range(1, amount + 1):
             for j in range(0, len(coins)):
                 if coins[j] <= i:
                     dp[i] = min(dp[i], dp[i - coins[j]] + 1)
-                    #the answer = min(the number from previous stage, #-the coin i have +1 i use the coin 1 time)
+                    #the answer = min(the number from previous stage, #-the coin i have +1)
         return -1 if dp[-1] > amount else dp[-1]
       
       #https://www.youtube.com/watch?v=jgiZlGzXMBw
