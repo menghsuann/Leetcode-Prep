a0336a43bdd0ef62c5ff2b53a7208b2ea7fcbd6d
Coco Tsai <menghsuantsai@berkeley.edu>
2021-04-08 17:32:11 -0400

    Create 23. Merge k Sorted Lists.py

---
+++ 23. Merge k Sorted Lists.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        nodes = []
+        #returning result
+        dummy = ListNode(-1)
+        head = dummy
+        #1. save all values in nodes by 遍歷給我們的linked list，把值全部存在一個新的list
+        for list in lists:
+            #avoid list is null
+            while list:
+                nodes.append(list.val)
+                #update the pointer
+                list = list.next
+        #2. sort the new linked list and save to another new list
+        for val in sorted(nodes):
+            head.next = ListNode(val)#returning a linked list; not only val
+            head = head.next #update pointer
+        #3. return the new linked list
+        return dummy.next
+  #https://www.youtube.com/watch?v=YC7jKAr8FJ0
