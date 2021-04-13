37dcdc2b28f82039b383aa54b3cad044571085e3
Coco Tsai <menghsuantsai@berkeley.edu>
2021-04-12 20:50:36 -0700

    Create 101. Symmetric Tree.py

---
+++ 101. Symmetric Tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        #extreme case
+        if root is None:
+            return True
+        #recursive function
+        return self.ifSym(root.left, root.right)
+    
+    def ifSym(self, left, right):
+        #extreme case still true
+        if left is None and right is None:
+            return True
+        #false condition
+        if left is None or right is None or left.val != right.val:
+            return False
+        #recursive function for child
+        return self.ifSym(left.left, right.right) and self.ifSym(left.right, right.left)
+        
+       #https://www.youtube.com/watch?v=ZXi6HLVe4uk
