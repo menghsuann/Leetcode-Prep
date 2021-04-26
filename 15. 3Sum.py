b340a9e8ef78ce95128d11273537a4f237059f57
Coco Tsai <menghsuantsai@berkeley.edu>
2021-04-26 01:21:18 -0700

    Update 15. 3Sum.py

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -2,28 +2,41 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         result = []
+        #sort first
         nums.sort()
+        #last three digit
         for i in range(n-2):
-            if nums[i]+nums[i+1]+nums[i+2] > 0:
+            #if all greater than zero
+            if nums[i] + nums[i+1] + nums[i+2] > 0:
                 break
-            if nums[i]+nums[n-2]+nums[n-1] < 0:
+            #if the smallest number is too small, cotinue to next indext directly
+            if nums[i] + nums[n-2] + nums[n-1] < 0:
                 continue
-            if 0 < i and nums[i] == nums[i-1]:
+            #if duplicate value
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
+            #兩頭往中間找
             l, r = i+1, n-1
+            #交叉後停止
             while l < r:
                 temp = nums[i] + nums[l] + nums[r]
+                #if = 0
                 if temp == 0:
                     result.append([nums[i], nums[l], nums[r]])
+                    #check if duplicate, update pointer
                     while l+1 < r and nums[l] == nums[l+1]:
                         l += 1
-                    l += 1
                     while l < r-1 and nums[r] == nums[r-1]:
                         r -= 1
+                    l += 1
                     r -= 1
+                #if < 0  
                 elif temp < 0:
                     l += 1
+                #if > 0
                 else:
                     r -= 1
         return result
         
+#https://www.youtube.com/watch?v=CW6G30xQCbw
+#https://www.youtube.com/watch?v=WOpdLrqzOOw
