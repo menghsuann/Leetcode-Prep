90ee1f4fefd2345349532ac0a38d3b837a8f0b50
Coco Tsai <menghsuantsai@berkeley.edu>
2021-04-27 01:06:59 -0700

    Update 49. Group Anagrams.py

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -1,11 +1,12 @@
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        #create a dic
         dic = defaultdict(list)
-        #sort and join the string and append back to dic
         for s in strs:
+            #at dic ate sorted category in append eat, tea, ate
             dic[''.join(sorted(s))].append(s)
+            #print(s)
         return dic.values()
+        
       #https://www.youtube.com/watch?v=BHC0vgpsl5k
       #https://www.youtube.com/watch?v=Ge04OM0r0d8&t=2s
