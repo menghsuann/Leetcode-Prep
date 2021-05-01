493b2ec20045c65e6e16e1346aa9a548559b78e9
Coco Tsai <menghsuantsai@berkeley.edu>
2021-05-01 16:23:27 -0700

    Update 70. Climbing Stairs.py

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -12,4 +12,6 @@ class Solution:
       #n=6, 1+1+1+1+1+1, 2+2+2, 1+1+2+2(6), 1+1+1+1+2(5) --> output = 13 = (5+8)
       #Fibonacci Sequence
     
- #https://www.youtube.com/watch?v=NgamBohlrf8
+#https://www.youtube.com/watch?v=NgamBohlrf8
+#If we can move 1, 2 and 3 steps each time. we only need to add up total ways of last 3 stairs together.
+#O(n) : We iterate all dp array, it will cost O(n), each value will add up last two value as result, it will cost (1+2), in total will be O(n+2n) --> O(n)
