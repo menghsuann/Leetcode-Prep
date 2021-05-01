6c42078afacf68372d3b233a84543cfecd11e6c1
Coco Tsai <menghsuantsai@berkeley.edu>
2021-05-01 00:08:42 -0700

    Create 1137. N-th Tribonacci Number.py

---
+++ 1137. N-th Tribonacci Number.py
@@ -0,0 +1,19 @@
+def printTribRec(n) :
+    if (n == 0 or n == 1 or n == 2) :
+        return 0
+    elif (n == 3) :
+        return 1
+    else :
+        return (printTribRec(n - 1) +
+                printTribRec(n - 2) +
+                printTribRec(n - 3))
+#O(3^N)
+
+def tribonacci(self, n: int) -> int:
+        dp = [0,1,1]+[0 for _ in range(3,n+1)]
+        for x in range(3,n+1):
+            dp[x] = dp[x-1]+dp[x-2]+dp[x-3]
+        return dp[n]
+#O(n)
+#create a dp list
+#add previous three values so the current value become to store the current result.
