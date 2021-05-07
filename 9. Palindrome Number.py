3c106c747e5dea6046bd9ef9fbd8b3473ae57a7d
Coco Tsai <menghsuantsai@berkeley.edu>
2021-05-06 17:40:10 -0700

    Update 9. Palindrome Number.py

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -3,9 +3,9 @@ class Solution:
         num = 0
         a = abs(x)
         while(a != 0):
-            temp = a % 10
-            num = num * 10 + temp
-            a = int(a / 10)  
+            temp = a % 10 #get the unit digit
+            num = num * 10 + temp #push num to the left to store unit digit from new temp
+            a = int(a / 10)  #divide the original a without unit digit
         if x >= 0 and x == num:
             return True
         else:
